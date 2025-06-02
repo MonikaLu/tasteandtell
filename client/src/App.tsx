f892eac213ee6b6a69896ff84f7d63a826038ff1
Monika Luu <monika-luu@hotmail.com>
2025-06-02 20:36:12 +0200

    client(styling): init shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1 className="text-2xl">Taste and Tell</h1>
       <p className="read-the-docs">Where every bite tells a story...</p>
+      <Button variant="outline">Click me</Button>
     </>
   );
 }
