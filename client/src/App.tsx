5e80fe4f0d6692b3362bcf5c9e6baa452fef53f9
Monika Luu <monika-luu@hotmail.com>
2025-06-02 20:08:23 +0200

    client: init repo

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Taste and Tell</h1>
+      <p className="read-the-docs">Where every bite tells a story </p>
+    </>
+  );
+}
+
+export default App;
