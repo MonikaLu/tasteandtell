a2539ef8c9a0c002e3b2388cb99b1afa3d46a718
Monika Luu <monika-luu@hotmail.com>
2025-06-02 20:21:47 +0200

    client(styling): add Tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Taste and Tell</h1>
-      <p className="read-the-docs">Where every bite tells a story </p>
+      <h1 className="text-2xl">Taste and Tell</h1>
+      <p className="read-the-docs">Where every bite tells a story...</p>
     </>
   );
 }
