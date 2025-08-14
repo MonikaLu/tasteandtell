dbd3ddca5e777e834b90ad9139fb905fb6934481
monikalu <monika-luu@hotmail.com>
2025-08-14 15:55:20 +0200

    add standard configuration for test setup

--- api/src/test/kotlin/ApplicationKtTest.kt
+++ api/src/test/kotlin/ApplicationKtTest.kt
@@ -1,2 +1,20 @@
-import org.junit.jupiter.api.Assertions.*
- class ApplicationKtTest
+import com.example.module
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ApplicationKtTest {
+     @Test
+     fun testRoot() = testApplication {
+         application {
+             module()
+         }
+
+         val response = client.get("/")
+         assertEquals(HttpStatusCode.OK, response.status)
+         assertEquals("Taste and Tell", response.bodyAsText())
+     }
+ }
