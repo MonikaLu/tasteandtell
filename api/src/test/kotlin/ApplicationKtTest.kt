30c110f1db7779a9fb6fac197987b00cab54adc8
monikalu <monika-luu@hotmail.com>
2025-09-07 18:13:38 +0200

    test: add setup for unit test

--- api/src/test/kotlin/ApplicationKtTest.kt
+++ api/src/test/kotlin/ApplicationKtTest.kt
@@ -1,20 +1,54 @@
+import com.example.models.Ingredient
+import com.example.models.Instruction
+import com.example.models.Recipe
 import com.example.module
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import org.junit.Assert.assertEquals
 import org.junit.Test
+import kotlin.test.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 
 class ApplicationKtTest {
      @Test
-     fun testRoot() = testApplication {
+     fun newRecipeCanBeAdded() = testApplication {
          application {
              module()
          }
+         val client = createClient {
+             install(ContentNegotiation) {
+                 json()
+             }
+         }
 
-         val response = client.get("/")
-         assertEquals(HttpStatusCode.OK, response.status)
-         assertEquals("Taste and Tell", response.bodyAsText())
+         val recipe =         Recipe(
+             "5", "Pasta aglio e olio", "Buy a bread",
+             createdAt = "2024-1-2",
+             updatedAt = "2024-1-2",
+             authorId = "1",
+             coverImageUrl = "",
+             ingredients = listOf(
+                 Ingredient(
+                     "1", "Bread", "testing", 1
+                 )
+             ),
+             instructions = listOf(
+                 Instruction(
+                     "1", 2, "test", "d"
+                 )
+             )
+         )
+
+         val response = client.post("/recipes") {
+             header(
+                 HttpHeaders.ContentType,ContentType.Application.Json,
+             )
+             setBody(recipe)
+         }
+         assertEquals(HttpStatusCode.Created, response.status)
+
+
      }
+
  }
