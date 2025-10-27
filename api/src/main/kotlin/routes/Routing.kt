fa08e13111bd8cef61ab9a1cef00741ed6ec61c3
monikalu <monika-luu@hotmail.com>
2025-10-27 15:56:51 +0100

    refactor code

--- api/src/main/kotlin/routes/Routing.kt
+++ api/src/main/kotlin/routes/Routing.kt
@@ -1,7 +1,5 @@
 package com.example.routes
 
-import com.example.models.Ingredient
-import com.example.models.Instruction
 import com.example.models.Recipe
 import com.example.repositories.RecipeRepository
 import io.ktor.http.*
@@ -9,86 +7,64 @@ import io.ktor.server.engine.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.SerializationException
 import java.time.LocalDateTime
 import java.util.*
+import kotlinx.serialization.SerializationException
 
 fun Route.recipeRoutes() {
-        route("/recipes") {
-            get{
-                val recipes =       RecipeRepository.allRecipes()
-                call.respond(
-                    recipes
-                )
+    route("/recipes") {
+        get {
+            val recipes = RecipeRepository.allRecipes()
+            call.respond(recipes)
+        }
+        get("/{id}") {
+            val id = call.parameters["id"]
+            if (id == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
             }
-            get("/{id}") {
-                val id = call.parameters["id"]
-                if (id == null) {
-                    call.respond(HttpStatusCode.BadRequest)
-                    return@get
-                }
-                val recipe = RecipeRepository.recipeById(id)
-                if (recipe == null) {
-                    call.respond(HttpStatusCode.NotFound)
-                    return@get
-                }
-                call.respond(recipe)
+            val recipe = RecipeRepository.recipeById(id)
+            if (recipe == null) {
+                call.respond(HttpStatusCode.NotFound)
+                return@get
             }
+            call.respond(recipe)
+        }
 
-            post {
-                try {
-                    val request = call.receive<Recipe>()
-                    val newRecipe = Recipe(
+        post {
+            try {
+                val request = call.receive<Recipe>()
+                val newRecipe =
+                    request.copy(
                         id = UUID.randomUUID().toString(),
-                        title = request.title,
-                        description = request.description,
-                        coverImageUrl = request.coverImageUrl,
                         createdAt = LocalDateTime.now().toString(),
                         updatedAt = LocalDateTime.now().toString(),
-                        authorId = request.authorId,
-                        ingredients = request.ingredients.map {
-                            Ingredient(
-                                id = it.id,
-                                name = it.name,
-                                description = it.description,
-                                amount = it.amount,
-                            )
-                        },
-                        instructions = request.instructions.map{
-                            Instruction(
-                                id = it.id,
-                                stepNumber = it.stepNumber,
-                                description = it.description,
-                                imageUrl = it.imageUrl,
-                            )
-                        }
                     )
-                    RecipeRepository.addRecipe(newRecipe)
-                    call.respond(HttpStatusCode.Created, newRecipe)
-                } catch (e: IllegalStateException) {
-                    println("Illegal Argument")
-                     e.printStackTrace()
+                RecipeRepository.addRecipe(newRecipe)
+                call.respond(HttpStatusCode.Created, newRecipe)
+            } catch (e: IllegalStateException) {
+                println("Illegal Argument")
+                e.printStackTrace()
+                call.respond(HttpStatusCode.BadRequest)
+            } catch (e: SerializationException) {
+                println("SERIALIZATION EXCEPTION")
+                e.printStackTrace()
 
-                    call.respond(HttpStatusCode.BadRequest)
-                } catch (e: SerializationException) {
-                    println("SERIALIZATIONEXCEPTION")
-                     e.printStackTrace()
-
-                    call.respond(HttpStatusCode.BadRequest)
-                }
-            }
-
-            delete  ("/{id}") {
-                val recipeId = call.parameters["id"]
-                if (recipeId == null) {
-                    call.respond(HttpStatusCode.BadRequest)
-                    return@delete
-                }
-                if (RecipeRepository.removeRecipe(recipeId)) {
-                    call.respond(HttpStatusCode.NoContent)
-                } else {
-                    call.respond(HttpStatusCode.NotFound)
-                }
+                call.respond(HttpStatusCode.BadRequest)
             }
         }
+
+        delete("/{id}") {
+            val recipeId = call.parameters["id"]
+            if (recipeId == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@delete
+            }
+            if (RecipeRepository.removeRecipe(recipeId)) {
+                call.respond(HttpStatusCode.NoContent)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+    }
 }
